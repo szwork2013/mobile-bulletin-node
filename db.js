9fa97e21992eadb672f8852349348254d41aa6ed
Mazi Muhlari <mazi.muhlari@gmail.com>
2015-03-10 16:05:46 +0200

    Modified the connection string

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'mongodb://mbdb:strongword@localhost:27000/mobilebulletin-prod?replicaSet=MobileBulletin'
+  'url' : 'mongodb://mbdb:strongword@localhost:27000/mobilebulletin-prod'
 }
