012d70b85651d1c3be2da392c521bd8f1808db80
Mazi Muhlari <mazi.muhlari@gmail.com>
2015-03-10 10:59:56 +0200

    Modified the connection string

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'mongodb://mobilebulletin-prod:P@%40sw0rd@localhost:27000/mobilebulletin-prod'
+  'url' : 'mongodb://mobilebulletin-prod:P%40ssw0rd@localhost:27000/mobilebulletin-prod'
 }
